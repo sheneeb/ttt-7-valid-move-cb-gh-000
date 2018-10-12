22f93eb77032f0c38eb0283fd04266ed74dcdf09
Sheneeb Sadhik <sadhik.sheneeb@gmx.com>
2018-10-12 15:24:29 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if index.between?(0,8) && position_taken?(board, index) == true
   true
-  if index.between?(0,8) && position_taken?(board, index) == false
+elsif index.between?(0,8) && position_taken?(board, index) == false
   false
   end
 end
