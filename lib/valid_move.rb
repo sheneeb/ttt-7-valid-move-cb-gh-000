c803c2b2b01ede590d2c7a1ca0487c55ffa5b2d7
Sheneeb Sadhik <sadhik.sheneeb@gmx.com>
2018-10-12 15:12:14 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,10 +8,10 @@ end
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == " " || board[index] == ""
-    false
-  elsif board[index] == nil
-    false
-  elsif board[index] == "X" || board[index] == "O"
     true
+  elsif board[index] == nil
+    true
+  elsif board[index] == "X" || board[index] == "O"
+    false
   end
 end
